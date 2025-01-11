eab82d4dfbce4c6c7665768741ccfffde38779a2
Matthew Matiness Silwanus <matthewsilwanus@gmail.com>
2025-01-11 18:33:18 +0700

    03-create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Folksmart'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A Minimarket built with Next.js and MongoDB'
